69f9476dc3bd05d4eae69e64593232dc8d911730
TataBakhturidze <bakhturidzetata@gmail.com>
2021-03-13 13:46:44 +0400

    update

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './mainPage.css'
+import {useTranslation} from 'react-i18next'
+import i18next from '../../languages/i18n'
 
 function MainPage() {
+
+    const {t} = useTranslation()
+
     return (
         <div className='mainPage'>
-            Hello Choose route
+            {t("hello choose route")}
         </div>
     )
 }
